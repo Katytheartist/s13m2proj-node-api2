0df51dfc3bee6a41168668f9142797fce5490b31
Katy Kane <katytheartist@gmail.com>
2024-04-30 13:41:06 -0500

    wip

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -6,7 +6,8 @@ const router = express.Router()
 router.get('/', (req, res) =>{
     Post.find()
     .then(found =>{
-        throw new Error('ouch')
+        //throw new Error('ouch')
+        res.json(found)
     }) .catch(err =>{
         res.status(500).json({
         message: "The posts information could not be retrieved",
@@ -16,15 +17,53 @@ router.get('/', (req, res) =>{
     })
 })
 
-router.get('/:id', (req, res) =>{
-
+router.get('/:id', async (req, res) =>{
+try{
+    //throw new Error('pathetic!')
+    const post = await Post.findById(req.params.id)
+    //console.log('--->', post)
+    if(!post){
+        res.status(404).json({
+            message: "The post with the specified ID does not exist"
+        })
+    } else {
+        res.json(post)
+    }
+} catch (err){
+    res.status(500).json({
+        message: "The post information could not be retrieved",
+        err: err.message,
+        stack: err.stack,
+    })
+}
 })
 
 router.post('/', (req, res) =>{
-
+    const {title, contents} = req.body
+    if(!contents || !title){
+        res.status(400).json({
+            message: "Please provide title and contents for the post"
+        })
+    } else {
+       // console.log('success!')
+       Post.insert({title, contents})
+       .then(({id})=>{
+        console.log(id)
+        return Post.findById(id)
+       })
+       .then(post =>{
+        res.status(201).json(post)
+       }) .catch(err =>{
+        res.status(500).json({
+            message: "There was an error while saving the post to the database",
+            err: err.message,
+            stack: err.stack,
+        })
+       })
+    }
 })
 
-router.delete('/:id', (req, res) =>{
+router.delete('/:id', async (req, res) =>{
 
 })
 
